c3be73826435c23d833362a022b0acbae8fe15c1
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2019-05-30 16:42:02 +0300

    Add billing and client static side app

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Payments from './Payments';
+
+class Header extends Component {
+  renderContent = () => {
+    const { auth } = this.props;
+    switch (auth) {
+      case null:
+        return 'still desiding';
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2" style={{ margin: '0 10px' }}>
+            Credits: {auth.credits}
+          </li>,
+          <li key="3">
+            <a href="/api/logout">Logout</a>
+          </li>
+        ];
+    }
+  };
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">
+            Emaily
+          </Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
